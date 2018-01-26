96b22bea783d5b37c907407829cb569a117fb039
Brushblaster <33086404+Brushblaster@users.noreply.github.com>
2018-01-26 13:39:03 +0100

    Update light.js

--- pi/light.js
+++ pi/light.js
@@ -1,4 +1,10 @@
-const wpi = require('node-wiringpi')
+const wpi = require('wiringpi')
 const io = require('socket.io')
 
+wpi.setup('wpi')
 
+wpi.pinMode(pin, wpi.PWM_OUTPUT)
+
+module.exports = {
+  
+}
