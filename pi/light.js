d5a3caa1ccac64431a3adbd6ec4a706787ec689c
Brushblaster <33086404+Brushblaster@users.noreply.github.com>
2018-01-29 08:38:10 +0100

    Update light.js

--- pi/light.js
+++ pi/light.js
@@ -1,46 +1,47 @@
-const wpi = require('wiring-pi')
+'strict'
+// requireing modules
+const five = require('johnny-five')
+const raspiConfig = require('../config/raspiConfig')
+const arduinoConfig = require('../config/arduinoConfig')
+const Raspi = require('raspi-io')
 
-wpi.setup('wpi')
 
 // Driving Motor
 
-module.exports = {
-  driveMotor: function (speed, steps, acc, start, pin) {
+exports = module.exports = function (io) {
+// init Socket.io
+  io.sockets.on('connection', function (socket) {
+    // init johnny-five board
+    const board = new five.Board()
+    board.on('ready', function () {
+      var channel1 = new five.Led(raspiConfig.pinChannel1)
+      var channel2 = new five.Led(raspiConfig.pinChannel2)
+      var channel3 = new five.Led(raspiConfig.pinChannel3)
+      var channel4 = new five.Led(raspiConfig.pinChannel4)
+      var channel5 = new five.Led(raspiConfig.pinChannel5)
+      var channel6 = new five.Led(raspiConfig.pinChannel6)
+      var channel7 = new five.Led(raspiConfig.pinChannel7)
+      var channel8 = new five.Led(raspiConfig.pinChannel8)
 
-    wpi.pinMode(pin, wpi.OUTPUT)
-
-    let timeout = 1200
-
-    for (let stepCount = 0; stepCount < steps; i++) {
-      wpi.digitalWrite(1)
-      wpi.delayMicroseconds(timeout)
-      wpi.digitalWrite(0)
-    }
-  }/* ,
-
-
-  setDirection: function (left, right, pin) {
-    const direction = gpio.GPIO
-
-    let cableCarDirection = new GPIO(pin)
-    cableCarDirection.open()
-    cableCarDirection.setMode(gpio.OUT)
-    if (left && !right) {
-      cableCarDirection.wirte(gpio.HIGH)
-    } else {
-      cableCarDirection.write(gpio.LOW)
-    }
-  },
-
-  setEnable: function (ena, pin) {
-    const enable = gpio.GPIO
-
-    let cableCarEna = new GPIO(pin)
-    cableCarEna.open()
-    cableCarEna.setMode(gpio.OUT)
+    // drive motor in x steps to the right
+    socket.on('channel1', function (data) {
+      cahnnel1.rpm(data.speed)
+        .ccw()
+        .accel(data.accel)
+        .decel(data.accel)
+        .step(data.steps, function () {
+          console.log('finished')
+        })
+    })
     
-    if (!ena) {
-      cableCarEna.write(gpio.HIGH)
-    }
-  } */
+        // stop Movement
+    socket.on('stop', function () {
+      console.log('stoping Drive...')
+      cableCarPilatusEna.off()
+      cableCarRigiEna.off()
+    })
+
+    
+    })
+  })
 }
